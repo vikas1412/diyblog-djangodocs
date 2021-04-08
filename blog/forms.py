7eaa3d5963fa52bcd9aa34493c8191945c03e09b
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-08 23:24:42 +0530

    Add SignupForm class

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
@@ -21,4 +23,13 @@ class AddAuthorForm(forms.Form):
 
         return date_of_birth
 
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=False, help_text="Enter first name")
+    last_name = forms.CharField(max_length=50, required=False, help_text="Enter last name")
+    email = forms.EmailField(max_length=200, help_text="Required. Inform a valid email address")
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
